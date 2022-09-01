936d092ef7bc2c26426337b45f55261ba25a638d
Joshua Campos <jdeveloper1203@gmail.com>
2022-09-01 16:56:01 -0400

    added the event listener to prevent default

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
+const tasks = document.getElementById('tasks');
+const button = document.querySelector('input[type="submit"]');
+let description = document.getElementById('new-task-description');
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    addTextToList();
+
+  });
 });
+
+function addTextToList() {
+  const li = document.createElement('li');
+  li.innerHTML = description.value;
+  tasks.appendChild(li);
+}
+
